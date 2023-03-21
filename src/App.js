a45c80b4a8c8bdef3f7abf5ff5ea1e4cd6186015
IliaIvashkevich <i.ivashkevich@list.ru>
2023-03-21 23:25:07 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Breaking changes!
-        Next.js 
-        Bootstrap
 
       
       </header>
